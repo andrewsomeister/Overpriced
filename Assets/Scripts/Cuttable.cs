84f47a085413cac8b871df90d993c57064fddc13
yiyuan <gladysfangcs@gmail.com>
2023-05-02 16:51:25 +0200

    added wait routine, now cutting into 4 slices and generate trash ball

--- Assets/Scripts/Cuttable.cs
+++ Assets/Scripts/Cuttable.cs
@@ -7,9 +7,11 @@ public class Cuttable : MonoBehaviour
     // Start is called before the first frame update
     [Header("Cut With ")] public GameObject cutWith; // object that can cut this object
     [Header("Cut Into")] public GameObject cutInto; // Prefab Only,slices, dices etc. 
+    [Header("Trash Prefab")] public GameObject trashPrefab; // Prefab Only,slices, dices etc.
     private int cutCount = 0;
     public AudioClip cutAudioClip; 
     public AudioClip destroyAudioCilp;
+    public int cutLimit = 4;  // cutting how many times will destroy the object 
     private AudioSource audioSource; // the object that can play the sound
 
     void Start()
@@ -23,19 +25,33 @@ public class Cuttable : MonoBehaviour
         
     }
 
+    private int destroyDelayTime = 3; 
+
     void OnTriggerEnter(Collider other ){
         if (other.gameObject.tag == "knife"){
             cutCount += 1; 
-            Debug.LogWarningFormat("knife cutting cuttable ", other.name); 
             audioSource.PlayOneShot(cutAudioClip);
+            Vector3 point3 = new Vector3(0, 2 * cutCount, 0);
             Instantiate(cutInto, transform.position, transform.rotation);
-            Debug.Log("tomato slice supposedly show up ");
-            if (cutCount == 4){
+            // cutting many times, destroy after 4 cuts
+            if (cutCount == cutLimit){
                 audioSource.PlayOneShot(destroyAudioCilp);
-                Destroy(gameObject);
+                // instantiate a trash object 
+                Instantiate(trashPrefab, transform.position, transform.rotation);
+            
+                Wait(destroyDelayTime); 
+                Debug.Log("Destroying Object");
+                gameObject.SetActive(false);
+                
             }
         }
     }
+
+    IEnumerator Wait(int seconds)
+{
+    Debug.Log("Inside wait function " + seconds); 
+    yield return new WaitForSeconds(seconds);
+}
 
 
 }
